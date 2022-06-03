6ca9a79796719fb05a47218f58bcf34ab87867e2
emortal <nathanpenwill88@gmail.com>
2022-06-03 22:53:06 +0100

    Made marathon slightly harder

--- src/main/kotlin/dev/emortal/marathon/generator/LegacyGenerator.kt
+++ src/main/kotlin/dev/emortal/marathon/generator/LegacyGenerator.kt
@@ -8,7 +8,7 @@ object LegacyGenerator : Generator() {
         val rand = ThreadLocalRandom.current()
 
         val y = if (targetY == 0) rand.nextInt(-1, 2) else if (targetY < pos.y()) -1 else 1
-        val z = if (y == 1) rand.nextInt(1, 4) else if (y == -1) rand.nextInt(1, 6) else rand.nextInt(1, 5)
+        val z = if (y == 1) rand.nextInt(1, 4) else if (y == -1) rand.nextInt(2, 6) else rand.nextInt(1, 5)
         val x = rand.nextInt(-3, 4)
 
         return pos.add(x.toDouble(), y.toDouble(), z.toDouble())
